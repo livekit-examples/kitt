caf4fd7af3f072f8287eeee3eefa4059ff6a9a49
Théo Monnom <theo.monnom@outlook.com>
2023-03-26 18:51:57 -0700

    ogg wip

--- lkgpt-service/go.mod
+++ lkgpt-service/go.mod
@@ -3,6 +3,7 @@ module github.com/livekit-examples/livegpt
 go 1.19
 
 require (
+	cloud.google.com/go/speech v1.15.0
 	github.com/livekit/protocol v1.5.1
 	github.com/livekit/server-sdk-go v1.0.8
 	github.com/pion/rtp v1.7.13
@@ -13,6 +14,10 @@ require (
 )
 
 require (
+	cloud.google.com/go v0.110.0 // indirect
+	cloud.google.com/go/compute v1.18.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/longrunning v0.4.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bep/debounce v1.2.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
@@ -23,8 +28,12 @@ require (
 	github.com/frostbyte73/core v0.0.4 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.7.1 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/jxskiss/base62 v1.1.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
@@ -57,13 +66,17 @@ require (
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/thoas/go-funk v0.9.3 // indirect
 	github.com/twitchtv/twirp v8.1.3+incompatible // indirect
+	go.opencensus.io v0.24.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.24.0 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/oauth2 v0.6.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
+	google.golang.org/api v0.114.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230320184635-7606e756e683 // indirect
 	google.golang.org/grpc v1.53.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
--- lkgpt-service/pkg/service/gptparticipant.go
+++ lkgpt-service/pkg/service/gptparticipant.go
@@ -1,15 +1,21 @@
 package service
 
 import (
+	"context"
 	"errors"
+	"io"
 	"strings"
 
+	speech "cloud.google.com/go/speech/apiv1"
+	"cloud.google.com/go/speech/apiv1/speechpb"
 	"github.com/livekit/protocol/auth"
 	"github.com/livekit/protocol/livekit"
 	lksdk "github.com/livekit/server-sdk-go"
 	"github.com/livekit/server-sdk-go/pkg/samplebuilder"
 	"github.com/pion/rtp/codecs"
 	"github.com/pion/webrtc/v3"
+
+	"github.com/pion/webrtc/v3/pkg/media/oggwriter"
 )
 
 var (
@@ -17,13 +23,14 @@ var (
 )
 
 type GPTParticipant struct {
-	room *lksdk.Room
+	room         *lksdk.Room
+	speechClient *speech.Client
 }
 
 func ConnectGPTParticipant(url, token string, roomService *lksdk.RoomServiceClient) (*GPTParticipant, error) {
 	roomCallback := &lksdk.RoomCallback{
 		ParticipantCallback: lksdk.ParticipantCallback{
-			OnTrackSubscribed: trackSubscribed,
+			OnTrackSubscribed: p.trackSubscribed,
 		},
 	}
 
@@ -33,52 +40,116 @@ func ConnectGPTParticipant(url, token string, roomService *lksdk.RoomServiceClie
 		return nil, err
 	}
 
+	ctx := context.Background()
+	speechClient, err := speech.NewClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+
 	return &GPTParticipant{
-		room: room,
+		room:         room,
+		speechClient: speechClient,
 	}, nil
 }
 
-func trackSubscribed(track *webrtc.TrackRemote, publication *lksdk.RemoteTrackPublication, rp *lksdk.RemoteParticipant) {
+func (p *GPTParticipant) trackSubscribed(track *webrtc.TrackRemote, publication *lksdk.RemoteTrackPublication, rp *lksdk.RemoteParticipant) {
 	// Transcribe the audio track
 	if track.Kind() != webrtc.RTPCodecTypeAudio {
 		return
 	}
 
-	transcriber, err := NewTranscriber(track)
+	transcriber, err := NewTranscriber(track, p.speechClient)
 	if err != nil {
 		return
 	}
 
-	go transcriber.Start()
+	go transcriber.start()
+}
+
+func (p *GPTParticipant) Disconnect() {
+	p.room.Disconnect()
+	p.speechClient.Close()
 }
 
 type Transcriber struct {
-	track *webrtc.TrackRemote
-	sb    *samplebuilder.SampleBuilder
+	track         *webrtc.TrackRemote
+	sampleBuilder *samplebuilder.SampleBuilder
+	oggReader     *io.PipeReader
+	oggWriter     *oggwriter.OggWriter
+	speechClient  *speech.Client
 }
 
-func NewTranscriber(track *webrtc.TrackRemote) (*Transcriber, error) {
+func NewTranscriber(track *webrtc.TrackRemote, speechClient *speech.Client) (*Transcriber, error) {
 	if !strings.EqualFold(track.Codec().MimeType, "audio/opus") {
 		return nil, errors.New("only opus is supported")
 	}
 
-	sb := samplebuilder.New(200, &codecs.OpusPacket{}, track.Codec().ClockRate)
+	rtpCodec := track.Codec()
+	sb := samplebuilder.New(200, &codecs.OpusPacket{}, rtpCodec.ClockRate)
+
+	oggReader, pw := io.Pipe()
+	oggWriter, err := oggwriter.NewWith(pw, rtpCodec.ClockRate, rtpCodec.Channels)
+	if err != nil {
+		return nil, err
+	}
+
 	return &Transcriber{
-		track: track,
-		sb:    sb,
+		track:         track,
+		sampleBuilder: sb,
+		oggReader:     oggReader,
+		oggWriter:     oggWriter,
+		speechClient:  speechClient,
 	}, nil
 }
 
-func (t *Transcriber) Start() {
+func (t *Transcriber) initSpeech(ctx context.Context) error {
+	stream, err := t.speechClient.StreamingRecognize(ctx)
+	if err != nil {
+		return err
+	}
+
+	rtpCodec := t.track.Codec()
+
+	// Send the initial configuration message.
+	if err := stream.Send(&speechpb.StreamingRecognizeRequest{
+		StreamingRequest: &speechpb.StreamingRecognizeRequest_StreamingConfig{
+			StreamingConfig: &speechpb.StreamingRecognitionConfig{
+				Config: &speechpb.RecognitionConfig{
+					Encoding:          speechpb.RecognitionConfig_OGG_OPUS,
+					SampleRateHertz:   int32(rtpCodec.ClockRate),
+					AudioChannelCount: int32(rtpCodec.Channels),
+					LanguageCode:      "en-US", // TODO(theomonnom) Support multiple languages
+				},
+			},
+		},
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *Transcriber) start() {
+	ctx := context.Background()
+	buf := make([]byte, 1024)
 	for {
 		pkt, _, err := t.track.ReadRTP()
 		if err != nil {
-			break
+			return
 		}
-		t.sb.Push(pkt)
+		t.sampleBuilder.Push(pkt)
 
-		for _, p := range t.sb.PopPackets() {
+		for _, p := range t.sampleBuilder.PopPackets() {
+			t.oggWriter.WriteRTP(p)
 
+			n, err := t.oggReader.Read(buf)
+			if err != nil {
+				return
+			}
+
+			if n > 0 {
+
+			}
 		}
 	}
 }
