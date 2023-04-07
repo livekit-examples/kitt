b9c946406ec1b5089ccc4fe0e552588b31a45589
Théo Monnom <theo.monnom@outlook.com>
2023-04-07 21:12:53 +0200

    remove meet seo

--- meet/pages/_app.tsx
+++ meet/pages/_app.tsx
@@ -6,54 +6,7 @@ import { DefaultSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <DefaultSeo
-        title="LiveKit Meet | Conference app build with LiveKit Open Source"
-        titleTemplate="%s"
-        defaultTitle="LiveKit Meet | Conference app build with LiveKit open source"
-        description="LiveKit is an open source WebRTC project that gives you everything needed to build scalable and real-time audio and/or video experiences in your applications."
-        twitter={{
-          handle: '@livekitted',
-          site: '@livekitted',
-          cardType: 'summary_large_image',
-        }}
-        openGraph={{
-          url: 'https://meet.livekit.io',
-          images: [
-            {
-              url: 'https://meet.livekit.io/images/livekit-meet-open-graph.png',
-              width: 2000,
-              height: 1000,
-              type: 'image/png',
-            },
-          ],
-          site_name: 'LiveKit Meet',
-        }}
-        additionalMetaTags={[
-          {
-            property: 'theme-color',
-            content: '#070707',
-          },
-        ]}
-        additionalLinkTags={[
-          {
-            rel: 'icon',
-            href: '/favicon.ico',
-          },
-          {
-            rel: 'apple-touch-icon',
-            href: '/images/livekit-apple-touch.png',
-            sizes: '180x180',
-          },
-          {
-            rel: 'mask-icon',
-            href: '/images/livekit-safari-pinned-tab.svg',
-            color: '#070707',
-          },
-        ]}
-      />
-      <Component {...pageProps} />
-    </>
+    <Component {...pageProps} />
   );
 }
 
