dbb9856ff23a8a41e4fec64134d6975ce7f8898c
Théo Monnom <theo.monnom@outlook.com>
2023-04-10 20:26:19 +0200

    Update completion.go

--- lkgpt-service/pkg/service/completion.go
+++ lkgpt-service/pkg/service/completion.go
@@ -43,7 +43,7 @@ func (c *ChatCompletion) Complete(ctx context.Context, history []*Sentence, prom
 				Content: "You are a voice assistant in a meeting named KITT, make concise/short answers. " +
 					"Finish your requests or questions using a question mark (?)" +
 					"Always prepend the language code you used before answering. " +
-					"e.g: fr-FR sentence in French..",
+					"e.g: en-US sentence in English..",
 			},
 			{
 				Role:    openai.ChatMessageRoleUser,
